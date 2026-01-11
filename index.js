3a709d6bb3579894187c40d6450db76329b57cbc
ejay-254 <ellacherop@gmail.com>
2026-01-11 17:05:01 +0100

    run the right tests

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+function combineUsers(...arrays) {
+    return {
+        date: new Date(),
+        users: [].concat(...arrays)
+    };
+}
 
+module.exports = combineUsers;
 
-
+module.exports = mergeUsers;
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
